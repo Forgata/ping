a7300631595f2cf9820ee4fb8c40b89531284482
Forgata <gombwadivine@gmail.com>
2026-04-29 10:56:09 +0200

    refactor createTargetSchema to encapsulate properties within a body object

--- src/modules/targets/target.schema.ts
+++ src/modules/targets/target.schema.ts
@@ -1,15 +1,19 @@
 import z from "zod";
 
 export const createTargetSchema = z.object({
-  name: z.string().min(1, "Name is required"),
-  url: z.url("Invalid URL format"),
-  intervalSeconds: z
-    .number()
-    .int()
-    .positive("Interval must be a positive integer"),
+  body: z.object({
+    name: z.string().min(1, "Name is required"),
+    url: z.url("Invalid URL format"),
+    intervalSeconds: z
+      .number()
+      .int()
+      .positive("Interval must be a positive integer"),
+  }),
 });
 
-export type CreateTargetInput = z.infer<typeof createTargetSchema>;
+export type CreateTargetInputBody = z.infer<
+  typeof createTargetSchema.shape.body
+>;
 
 export const deleteTargetSchema = z.object({
   params: z.object({ id: z.string().length(24, "Invalid target ID") }),
@@ -18,4 +22,3 @@ export const deleteTargetSchema = z.object({
 export type DeleteTargetParams = z.infer<
   typeof deleteTargetSchema.shape.params
 >;
-export type DeleteTargetInput = { params: DeleteTargetParams };
