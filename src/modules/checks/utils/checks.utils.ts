3f415ebd9d4bf44751fbcb43007312f327222b25
Forgata <gombwadivine@gmail.com>
2026-04-30 09:43:45 +0200

    refactor name checkCount

--- src/modules/checks/utils/checks.utils.ts
+++ src/modules/checks/utils/checks.utils.ts
@@ -2,12 +2,12 @@ import type { ICheck } from "../check.model.js";
 
 export const getUptime = (targets: ICheck[]) => {
   const successCount = targets.filter((target) => target.success).length;
-  const totalCount = targets.length;
-  const uptime = (successCount / totalCount) * 100;
-  return { totalCount, uptime };
+  const totalCheckCount = targets.length;
+  const uptime = (successCount / totalCheckCount) * 100;
+  return { totalCheckCount, uptime };
 };
 
-export const avgLatency = (targets: ICheck[]) => {
+export const getLatency = (targets: ICheck[]) => {
   const totalLatency = targets.reduce(
     (acc, target) => acc + target.latencyMs,
     0,
