6796b9869c2ceab8a7516d44c6c68b71505b5c43
Forgata <gombwadivine@gmail.com>
2026-05-17 15:56:27 +0200

    add type safe fetch util

---
+++ packages/frontend/utils/fetch.ts
@@ -0,0 +1,20 @@
+import { Document } from "mongoose";
+import next from "next/dist/types";
+
+interface FetchData<T> {
+  success: boolean;
+  data: T extends never[]
+    ? Omit<T[number], keyof Document>
+    : Omit<T, keyof Document>;
+}
+export async function fetchData<T>(url: string): Promise<FetchData<T>> {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+    const data: FetchData<T> = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Fetch error:", error);
+    throw error;
+  }
+}
