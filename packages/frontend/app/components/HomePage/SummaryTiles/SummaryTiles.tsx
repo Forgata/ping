1a996cdb6f77dee54647da75febff844dbab04a8
Forgata <gombwadivine@gmail.com>
2026-05-19 13:15:39 +0200

    refactor SummaryTilesUI component to use props for dynamic data rendering

--- packages/frontend/app/components/HomePage/SummaryTiles/SummaryTiles.tsx
+++ packages/frontend/app/components/HomePage/SummaryTiles/SummaryTiles.tsx
@@ -1,40 +1,47 @@
 "use client";
-
-export default function SummaryTilesUI() {
+interface SummaryTileProps {
+  UpCountStatus: {
+    upCount: number;
+    percentage: number;
+  };
+  downCount: number;
+  activeAlerts: number;
+  targets: number;
+}
+export default function SummaryTilesUI({
+  UpCountStatus,
+  downCount,
+  activeAlerts,
+  targets,
+}: SummaryTileProps) {
   return (
-    <div className="grid grid-col-1 md:grid-cols-5 gap-px bg-gray-950 mb-6">
+    <div className="grid grid-col-1 md:grid-cols-4 gap-4 mb-6">
       <div className="bg-gray-800 p-4">
         <p className="font-semibold capitalize">Total Targets</p>
         <div className="flex items-baseline gap-2">
-          <span>24</span>
+          <span>{targets}</span>
           <span>Nodes</span>
         </div>
       </div>
       <div className="bg-gray-800 p-4 border border-gray-950">
         <p className="font-semibold capitalize">Up count</p>
         <div className="flex items-baseline gap-2">
-          <span>22</span>
-          <span>91.66%</span>
+          <span>{UpCountStatus.upCount}</span>
+          <span>{UpCountStatus.percentage.toFixed(2)}%</span>
         </div>
       </div>
       <div className="bg-gray-800 p-4 border border-gray-950">
         <p className="font-semibold capitalize">Down Count</p>
         <div className="flex items-baseline gap-2">
-          <span>2</span>
+          <span>{downCount}</span>
           <span>Critical</span>
         </div>
       </div>
-      <div className="bg-gray-800 p-4 border border-gray-950">
-        <p className="font-semibold capitalize">Avg Latency</p>
-        <div className="flex items-baseline gap-2">
-          <span>48</span>
-          <span>ms</span>
-        </div>
-      </div>
+
       <div className="bg-gray-800 p-4 border border-gray-950">
         <p className="font-semibold capitalize">Active Alerts</p>
         <div className="flex items-baseline gap-2">
-          <span>2</span>
+          <span>{activeAlerts}</span>
           <span>Unresolved</span>
         </div>
       </div>
