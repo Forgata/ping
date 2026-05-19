911ef040698338374af9fa91a72bbcb4b8b43439
Forgata <gombwadivine@gmail.com>
2026-05-19 13:13:10 +0200

    add data fetching for targets and alerts in HeatMapGridContainer

--- packages/frontend/app/components/HomePage/HeatMapGrid/index.tsx
+++ packages/frontend/app/components/HomePage/HeatMapGrid/index.tsx
@@ -1,5 +1,16 @@
+import { fetchData } from "@utils/fetch";
 import HeatMapGridUI from "./HeatMap";
+import { ITarget } from "@backend/modules/targets/target.model";
+import { IAlert } from "@backend/modules/alerts/alert.model";
 
-export default function HeatMapGridContainer() {
+export default async function HeatMapGridContainer() {
+  const { data: targets } = await fetchData<ITarget[]>("/api/targets");
+  const { data: alerts } = await fetchData<IAlert[]>(
+    "http://localhost:3001/api/alerts",
+  );
+  const activeAlerts =
+    alerts?.filter((alert) => alert.status === "active").length || 0;
+  const upCount = targets.length - activeAlerts;
+
   return <HeatMapGridUI />;
 }
