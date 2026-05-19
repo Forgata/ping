e8253fcdbba27d78e2e737de68796e3ad6c7465d
Forgata <gombwadivine@gmail.com>
2026-05-19 13:14:06 +0200

    add data fetching and state management for SummaryTiles component

--- packages/frontend/app/components/HomePage/SummaryTiles/index.tsx
+++ packages/frontend/app/components/HomePage/SummaryTiles/index.tsx
@@ -1,5 +1,38 @@
+import { fetchData } from "@utils/fetch";
 import SummaryTilesUI from "./SummaryTiles";
+import { ITarget } from "@modules/targets/target.model";
+import { IAlert } from "@modules/alerts/alert.model";
 
-export default function SummaryTilesContainer() {
-  return <SummaryTilesUI />;
+export default async function SummaryTilesContainer() {
+  const { data: targets } = await fetchData<ITarget[]>(
+    "http://localhost:3001/api/targets",
+  );
+
+  const { data: alerts } = await fetchData<IAlert[]>(
+    "http://localhost:3001/api/alerts",
+  );
+
+  const activeAlerts =
+    alerts?.filter((alert) => alert.status === "active").length || 0;
+
+  const upCountStatus = {
+    upCount: targets.length - activeAlerts,
+    percentage:
+      activeAlerts > 0
+        ? ((targets.length - activeAlerts) / targets.length) * 100
+        : 100,
+  };
+
+  const downCount = alerts.filter(
+    (alert) => alert.message === "Service is down",
+  ).length;
+
+  return (
+    <SummaryTilesUI
+      targets={targets.length || 0}
+      activeAlerts={activeAlerts}
+      downCount={downCount}
+      UpCountStatus={upCountStatus}
+    />
+  );
 }
