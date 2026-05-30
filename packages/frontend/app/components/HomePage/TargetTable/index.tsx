b02abd07e45b1fac2ca102041465426df4b56f8d
Forgata <gombwadivine@gmail.com>
2026-05-30 16:29:28 +0200

    refactor: implement data fetching in TargetTableContainer for target data display

--- packages/frontend/app/components/HomePage/TargetTable/index.tsx
+++ packages/frontend/app/components/HomePage/TargetTable/index.tsx
@@ -1,4 +1,17 @@
+import { fetchData } from "@utils/fetch";
 import TargetTableUI from "./TargetTable";
-export default function TargetTableContainer() {
-  return <TargetTableUI />;
+import { ITarget } from "@backend/modules/targets/target.model";
+
+// interface TargetPageProps {
+//   searchParams: Promise<{ page?: string }>;
+// }
+export default async function TargetTableContainer() {
+  const { data: targets } = await fetchData<ITarget[]>(
+    "http://localhost:3001/api/targets",
+  );
+
+  // const params = await searchParams;
+  // const currentPage = Number(params.page) || 1;
+
+  return <TargetTableUI targets={targets} />;
 }
